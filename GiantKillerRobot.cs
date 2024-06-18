0a4386b1a042a676f6b1d016dd933137c21f5a2d
The Internet <The Internet@DESKTOP-FBPUOTL>
2024-06-18 18:45:26 +0300

    Add project files.

---
+++ GiantKillerRobot.cs
@@ -0,0 +1,65 @@
+﻿internal class GiantKillerRobot
+{
+    public bool Active { get; private set; }
+    public Intensity EyeLaserIntensity { get; set; }
+    public List<Target> Targets { get; private set; }
+    public int CurrentTarget { get; private set; }
+    public bool IsAlive { get; private set; }
+
+    public GiantKillerRobot()
+    {
+        Targets = new List<Target>();
+    }
+
+    public void Initialize()
+    {
+        Console.WriteLine("The robot is initializing.");
+        Active = true;
+        CurrentTarget = -1;
+    }
+
+    public void FireLaserAt(int targetIndex)
+    {
+        Console.WriteLine($"Firing laser at : {Targets[targetIndex]}");
+        Console.WriteLine();
+        IsAlive = false;
+    }
+
+    public void AcquireNextTarget()
+    {
+        CurrentTarget++;
+        if (CurrentTarget < Targets.Count)
+        {
+            Console.WriteLine($"Acquiring target : {Targets[CurrentTarget]}");
+            IsAlive = true;
+        }
+        else
+        {
+            Console.WriteLine($"No targets left");
+            Active = false;
+        }
+    }
+}
+class Planet
+{
+    public string Name { get; set; } = " ";
+    public bool ContainsLife { get; set; } = true;
+}
+enum Planets
+{
+    Earth,
+    Mars,
+    Saturn
+}
+enum Intensity
+{
+    Kill,
+    Freeze
+}
+enum Target
+{
+    Animal,
+    Human,
+    Superhero
+}
+
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿GiantKillerRobot robot = new GiantKillerRobot();
+robot.Initialize();
+robot.EyeLaserIntensity = Intensity.Kill;
+
+robot.Targets.Add(Target.Human);
+robot.Targets.Add(Target.Human);
+robot.Targets.Add(Target.Animal);
+robot.Targets.Add(Target.Animal);
+robot.Targets.Add(Target.Superhero);
+robot.Targets.Add(Target.Superhero);
+robot.Targets.Add(Target.Superhero);
+
+Planet earth = new Planet();
+earth.Name = Planets.Earth.ToString();
+
+while (robot.Active && earth.ContainsLife)
+{
+    if (robot.CurrentTarget < robot.Targets.Count && robot.IsAlive)
+        robot.FireLaserAt(robot.CurrentTarget);
+    else
+        robot.AcquireNextTarget();
+}
